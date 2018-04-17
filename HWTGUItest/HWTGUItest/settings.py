dd89d9629430774de9bfa8ace7408f356651f0c6
Austin <austin.coleman@ttu.edu>
2018-04-17 16:12:55 -0500

    Testing production phase

--- HWTGUItest/HWTGUItest/settings.py
+++ HWTGUItest/HWTGUItest/settings.py
@@ -19,12 +19,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'p@2_9!2^$_f4+bx3$&z3=5pvuj$mqlh5%7o$ota#!b+ood+a%0'
+with open('/home/aucolema/secret_key.txt') as f:
+    SECRET_KEY = f.read().strip()
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.ttu.edu']
 
 
 # Application definition
